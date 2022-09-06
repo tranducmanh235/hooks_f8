b07cd5f1ffcc5476329d0760ee2e94c16bd8eff3
Tran Duc Manh <tranducmanh235@gmail.com>
2022-09-06 12:52:24 +0700

    mounted or unmounted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import HooksUseState from "./components/HooksUseState";
 import Todolist_ from "./components/Todolist_";
+import Content from "./components/Content";
+import { useState } from "react";
 
 function App() {
+    const [show, setShow] = useState(false);
+
     return (
         <div className="App">
             {/* <HooksUseState /> */}
-            <div className="content">
-                <Todolist_ />
+            <div className="content" style={{ padding: 30 }}>
+                {/* <Todolist_ /> */}
+                <button onClick={() => setShow(!show)}>Toggle</button>
+                {show && <Content />}
             </div>
         </div>
     );
---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Content = () => {
+    // Mounted || Unmounted
+
+    return (
+        <div>
+            <h1>Hello everybody!</h1>
+        </div>
+    );
+};
+
+export default Content;
